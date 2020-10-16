bb6c40881a92fda02979269aadfbf983bbecde0e
Dencho96 <denysvk.96@gmail.com>
2020-10-16 16:32:56 +0300

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -21,9 +21,6 @@ gulp.task('sass', function() {
 
 gulp.task('script', function() {
     return gulp.src([
-            'node_modules/slick-carousel/slick/slick.js',
-            'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-            'node_modules/mixitup/dist/mixitup.js'
         ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
@@ -32,9 +29,7 @@ gulp.task('script', function() {
 
 gulp.task('style', function() {
     return gulp.src([
-            'node_modules/normalize.css/normalize.css',
-            'node_modules/slick-carousel/slick/slick.css',
-            'node_modules/magnific-popup/dist/magnific-popup.css'
+            'node_modules/normalize.css/normalize.css'
         ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -66,4 +61,4 @@ gulp.task('watch', function() {
     gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
 
-gulp.task('default', gulp.parallel('style', 'script', 'sass', 'watch', 'browser-sync'))
+gulp.task('default', gulp.parallel('style', 'sass', 'watch', 'browser-sync'))
